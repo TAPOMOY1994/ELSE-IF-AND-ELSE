f7174f582752b72e70d33677cf858227e0c9fe4d
Tapomoy Das <160303114+TAPOMOY1994@users.noreply.github.com>
2024-03-29 14:48:53 +0530

    Add files via upload

---
+++ ELSE IF AND ELSE.js
@@ -0,0 +1,56 @@
+let n=21;
+
+if(n===22)
+{
+    console.log("Cool");
+}
+
+else if(n===21)
+
+{
+    console.log("Good");
+}
+
+else
+
+{
+    console.log("Bad");
+}
+
+//Next one//
+
+
+const dayOfWeek=prompt("Enter a day");
+
+if (dayOfWeek==="Monday")
+
+{
+
+    console.log("SAD DAY");
+}
+
+else if(dayOfWeek==="Tuesday")
+
+{
+    console.log("SADD DAY");
+
+}
+
+
+else if(dayOfWeek==="Wednesday")
+{
+    console.log("MEDIOCRE DAY");
+}
+
+else if(dayOfWeek==="Thursday")
+
+{
+    console.log("GOOD DAY");
+}
+
+else
+
+{
+    console.log("GREAT DAY");
+}
+
